0c2e58c1e1bb2aef930337a47124ea0049321c87
mr-chidex <mbahchiboy2@gmail.com>
2023-02-14 12:28:38 +0100

    mongoose db integrated

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ const config = {
   API_VERSION: process.env.API_VERSION!,
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV!,
+  DATABASE_URL: process.env.DATABASE_URL!,
 };
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
+import mongoose from 'mongoose';
+
 import app from './app';
 import config from './config';
 
 const PORT = config.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server running on PORT:: 🚀💥>>> ${PORT}`));
+// connect database and start server
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(config.DATABASE_URL)
+  .then(() => {
+    console.log('db successfully connected');
+
+    // start server
+    app.listen(PORT, () => console.log(`server running on PORT:: 🚀💥>>> ${PORT}`));
+  })
+  .catch((_error) => {
+    console.log('error connecting to database');
+  });
